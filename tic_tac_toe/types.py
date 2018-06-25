4a074bfe33444110248138d90e455c5f0dfd9b5b
Vikas Munshi <vikas.munshi@ing.com>
2018-06-26 03:42:16 +0800

    added __repr__ for Cell and Player

--- tic_tac_toe/types.py
+++ tic_tac_toe/types.py
@@ -7,12 +7,14 @@ from typing import Callable, Generator, Tuple, Union
 
 Board = namedtuple('Board', ('size', 'moves'))
 Cell = namedtuple('Cell', ('row_id', 'col_id'))
+Cell.__repr__ = lambda self: 'c{}{}'.format(self.row_id, self.col_id)
 Cells = Tuple[Cell, ...]
 Game = namedtuple('Game', ['moves', 'result'])
 Games = Tuple[Game, ...]
 Lines = Tuple[Cells, ...]
 Moves = Cells
 Player = namedtuple('Player', ('name', 'strategy'))
+Player.__repr__ = lambda self: self.name
 Players = Tuple[Player, ...]
 Score = namedtuple('Score', ('player', 'points', 'wins', 'draws', 'losses', 'games', 'penalties'))
 Scores = Tuple[Score, ...]
