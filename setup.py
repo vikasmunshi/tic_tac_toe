9c0faa405932968373d929fdb9cc3eade52ab663
Vikas Munshi <vikas.munshi@ing.com>
2018-06-26 03:28:52 +0800

    reloacted files

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         url='https://github.com/vikasmunshi/python-scripts.git',
         description='Tic Tac Toe Tornament',
         packages=['tic_tac_toe', 'tic_tac_toe.strategies'],
-        package_dir={'tic_tac_toe': 'src'},
+        package_dir={'tic_tac_toe': 'tic_tac_toe'},
         install_requires=[],
         license='MIT License',
         platforms=['any'],
