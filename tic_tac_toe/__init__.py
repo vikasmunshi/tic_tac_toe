838f33e8b17e5e2b1b9e5c8654327b4aa40e8e22
Vikas Munshi <vikas.munshi@ing.com>
2018-07-04 23:03:42 +0200

    removed memory

--- tic_tac_toe/__init__.py
+++ tic_tac_toe/__init__.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 #   tic_tac_toe/__init__.py
 from .core import get_cells, get_lines, get_possible_moves, last_move_has_won, strategy
-from .game_graph import suggest_moves_graph
-from .game_memory import operator, suggest_moves_memory
-from .game_nn import suggest_move_nn
 from .tournament import play_tournament_eliminate, play_tournament_points
 from .user_types import Board, Cell, Cells, Lines, Player, Players, Scores
 from .util import cached, select_random_cell
@@ -17,8 +14,5 @@ __all__ = (
     'play_tournament_eliminate', 'play_tournament_points',
     'Board', 'Cell', 'Cells', 'Lines', 'Player', 'Players', 'Scores',
     'cached', 'select_random_cell',
-    'operator', 'suggest_moves_memory',
-    'suggest_moves_graph',
-    'suggest_move_nn',
     'memoize'
 )
