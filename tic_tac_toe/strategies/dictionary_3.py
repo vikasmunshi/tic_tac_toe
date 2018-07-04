56e74d6f320d0e503e724f4c5b065bb340084d42
Vikas Munshi <vikas.munshi@ing.com>
2018-07-04 17:42:28 +0200

    updated dictionary_3

--- tic_tac_toe/strategies/dictionary_3.py
+++ tic_tac_toe/strategies/dictionary_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# tic_tac_toe/strategies/hard_coded_3.py
+# tic_tac_toe/strategies/dictionary_3.py
 import json
 import os.path
 import string
@@ -8,7 +8,7 @@ import string
 from tic_tac_toe import *
 
 board_size = 3
-board_3 = {}
+board_3x3 = {}
 
 
 @cached
@@ -18,23 +18,26 @@ def cells_to_char(cells: Cells) -> str:
 
 @cached
 def char_to_cell(char: str) -> Cell:
-    if char:
-        cell_num = string.ascii_lowercase.index(char)
-        return Cell(row_id=cell_num // board_size, col_id=cell_num % board_size)
-    return ()
+    return Cell(row_id=char_to_cell_num(char) // board_size, col_id=char_to_cell_num(char) % board_size) if char else ()
+
+
+@cached
+def char_to_cell_num(char: str) -> int:
+    return string.ascii_lowercase.index(char)
 
 
 @cached
 def dict_load() -> None:
-    global board_3
+    global board_3x3
     fn = os.path.abspath(os.path.splitext(__file__)[0] + '.json')
     if os.path.exists(fn):
         with open(fn) as infile:
-            board_3 = json.load(infile)
+            board_3x3 = json.load(infile)
 
 
 def strategy(board: Board) -> Cell:
-    return char_to_cell(select_random_cell(board_3[cells_to_char(board.moves)]))
+    return char_to_cell(select_random_cell(board_3x3.get(cells_to_char(board.moves)) or get_possible_moves(board))) \
+        if board.size == board_size else select_random_cell(get_possible_moves(board))
 
 
 dict_load()
