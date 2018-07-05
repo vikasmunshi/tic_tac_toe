fab51da74722c7db5047a8b5016acc52f37a8a39
Vikas Munshi <vikas.munshi@ing.com>
2018-07-05 02:21:24 +0200

    updated dictionary

--- tic_tac_toe/strategies/ai_n.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#   tic_tac_toe/strategies/ai_n.py
-# from tic_tac_toe import *
-#
-#
-# def strategy(board: Board) -> Cell:
-#     return suggest_move_nn(board, get_possible_moves(board))
--- tic_tac_toe/strategies/random2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# from tic_tac_toe import *
-#
-#
-# def strategy(board):
-#     return select_random_cell(get_possible_moves(board))
